0f404f30d6ce6387e20c5666630c99db7c3ea6b9
Toon Vanhoutte <toon.vanhoutte@gmail.com>
2022-12-30 11:05:21 +0100

    Add App Config with managed identity

--- src/hotels-app/Ftw.Hotels.HotelPricing/Program.cs
+++ src/hotels-app/Ftw.Hotels.HotelPricing/Program.cs
@@ -1,6 +1,7 @@
 using StackExchange.Redis;
 using Ftw.Hotels.Common.Constants;
 using Ftw.Hotels.HotelPricing.Api.GraphQL;
+using Azure.Identity;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -9,13 +10,22 @@ builder.Configuration.AddJsonFile("appsettings.local.json", optional: true);
 builder.Configuration.AddEnvironmentVariables();
 #if DEBUG
 #else
-builder.Configuration.AddAzureAppConfiguration(builder.Configuration["APPCONFIG--CONNECTIONSTRING"]);
+builder.Configuration.AddAzureAppConfiguration(options =>
+{
+    var credential = new ChainedTokenCredential(new AzureCliCredential(), new ManagedIdentityCredential());
+    var uri = new Uri(builder.Configuration["APPCONFIG:URI"]);
+    var refreshRate = Convert.ToDouble(builder.Configuration["APPCONFIG:REFRESHRATE_IN_SECONDS"]);
+
+    options.Connect(uri, credential);
+    options.ConfigureKeyVault(keyVault => keyVault.SetCredential(credential));
+    options.UseFeatureFlags(flagOptions => flagOptions.CacheExpirationInterval = TimeSpan.FromSeconds(refreshRate));
+});
 #endif
 
 builder.Services
 #if DEBUG
 #else
-    .AddSingleton(ConnectionMultiplexer.Connect(builder.Configuration["REDIS--CONNECTIONSTRING"]))
+    .AddSingleton(ConnectionMultiplexer.Connect(builder.Configuration["REDIS:CONNECTIONSTRING"]))
 #endif
     .AddGraphQLServer()
     .AddQueryType<Query>()
