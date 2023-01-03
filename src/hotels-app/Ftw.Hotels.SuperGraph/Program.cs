cf1b13fbc4b84fe98bcca0e9c0e2e0d12f2c1bf8
Toon Vanhoutte <toon.vanhoutte@gmail.com>
2023-01-03 09:00:36 +0100

    Restructure configuration of SuperGraph

--- src/hotels-app/Ftw.Hotels.SuperGraph/Program.cs
+++ src/hotels-app/Ftw.Hotels.SuperGraph/Program.cs
@@ -5,39 +5,40 @@ using StackExchange.Redis;
 using Ftw.Hotels.Common.Constants;
 using Ftw.Hotels.SuperGraph.Api.GraphQL;
 using static HotChocolate.ErrorCodes;
+using Ftw.Hotels.Common.WebAppBuilderExtensions;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddHttpClient(SchemaNames.Local.HotelWeather,  c => c.BaseAddress = new Uri(builder.Configuration["API_HOTELWEATHER_URI"]));
-builder.Services.AddHttpClient(SchemaNames.Remote.HotelCatalog, c => c.BaseAddress = new Uri(builder.Configuration["API_HOTELCATALOG_URI"]));
-builder.Services.AddHttpClient(SchemaNames.Remote.HotelPricing, c => c.BaseAddress = new Uri(builder.Configuration["API_HOTELPRICING_URI"]));
-builder.Services.AddHttpClient(SchemaNames.Remote.HotelBooking, c => c.BaseAddress = new Uri(builder.Configuration["API_HOTELBOOKING_URI"]));
+#if DEBUG
+    builder.Configuration.ConfigureConfiguration(runLocal: true);
+#else
+    builder.Configuration.ConfigureConfiguration(runLocal: false);
+#endif
 
-if (builder.Environment.IsDevelopment())
-{
-    builder.Services
-        .AddGraphQL(SchemaNames.Local.HotelWeather)
-        .AddQueryType<Query>()
-        .AddGraphQLServer()
-        .AddRemoteSchema(SchemaNames.Remote.HotelCatalog)
-        .AddRemoteSchema(SchemaNames.Remote.HotelPricing)
-        .AddRemoteSchema(SchemaNames.Remote.HotelBooking)
-        .AddLocalSchema(SchemaNames.Local.HotelWeather)
-        .AddTypeExtensionsFromFile("./Api/Stitching/HotelWeatherExtension.graphql");
-}
-else
-{
-    builder.Services.AddSingleton(ConnectionMultiplexer.Connect(builder.Configuration["REDIS_CONNECTIONSTRING"]));
-    builder.Services
-        .AddGraphQL(SchemaNames.Local.HotelWeather)
-        .AddQueryType<Query>()
-        .AddGraphQLServer()
-        .AddRemoteSchemasFromRedis(SchemaNames.Remote.HotelCatalog, sp => sp.GetRequiredService<ConnectionMultiplexer>())
-        .AddRemoteSchemasFromRedis(SchemaNames.Remote.HotelPricing, sp => sp.GetRequiredService<ConnectionMultiplexer>())
-        .AddRemoteSchemasFromRedis(SchemaNames.Remote.HotelBooking, sp => sp.GetRequiredService<ConnectionMultiplexer>())
-        .AddLocalSchema(SchemaNames.Local.HotelWeather)
-        .AddTypeExtensionsFromFile("./Api/Stitching/HotelWeatherExtension.graphql");
-}
+builder.Services.AddHttpClient(SchemaNames.Local.HotelWeather,  c => c.BaseAddress = new Uri($"{builder.Configuration["API:HOTELWEATHER:URI"]}"));
+builder.Services.AddHttpClient(SchemaNames.Remote.HotelCatalog, c => c.BaseAddress = new Uri($"{builder.Configuration["API:HOTELCATALOG:URI"]}/graphql/"));
+builder.Services.AddHttpClient(SchemaNames.Remote.HotelPricing, c => c.BaseAddress = new Uri($"{builder.Configuration["API:HOTELPRICING:URI"]}/graphql/"));
+builder.Services.AddHttpClient(SchemaNames.Remote.HotelBooking, c => c.BaseAddress = new Uri($"{builder.Configuration["API:HOTELBOOKING:URI"]}/graphql/"));
+
+builder.Services
+#if DEBUG
+#else
+    .AddSingleton(ConnectionMultiplexer.Connect(builder.Configuration["REDIS:CONNECTIONSTRING"]))
+#endif
+    .AddGraphQL(SchemaNames.Local.HotelWeather)
+    .AddQueryType<Query>()
+    .AddGraphQLServer()
+#if DEBUG
+    .AddRemoteSchema(SchemaNames.Remote.HotelCatalog)
+    .AddRemoteSchema(SchemaNames.Remote.HotelPricing)
+    .AddRemoteSchema(SchemaNames.Remote.HotelBooking)
+#else
+    .AddRemoteSchemasFromRedis(SchemaNames.Remote.HotelCatalog, sp => sp.GetRequiredService<ConnectionMultiplexer>())
+    .AddRemoteSchemasFromRedis(SchemaNames.Remote.HotelPricing, sp => sp.GetRequiredService<ConnectionMultiplexer>())
+    .AddRemoteSchemasFromRedis(SchemaNames.Remote.HotelBooking, sp => sp.GetRequiredService<ConnectionMultiplexer>())
+#endif
+    .AddLocalSchema(SchemaNames.Local.HotelWeather)
+    .AddTypeExtensionsFromFile("./Api/Stitching/HotelWeatherExtension.graphql");
 
 var app = builder.Build();
 
