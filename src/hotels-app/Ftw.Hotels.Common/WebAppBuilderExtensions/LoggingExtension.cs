e8fea6d4f37c913055c54af29d869b8ea9a6b7f0
Toon Vanhoutte <toon.vanhoutte@gmail.com>
2023-02-10 17:08:50 +0100

    add logging extension

---
+++ src/hotels-app/Ftw.Hotels.Common/WebAppBuilderExtensions/LoggingExtension.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using OpenTelemetry.Resources;
+using OpenTelemetry.Trace;
+using Azure.Monitor.OpenTelemetry.Exporter;
+using Microsoft.Extensions.Configuration;
+
+namespace Ftw.Hotels.Common.WebAppBuilderExtensions
+{
+    public static class LoggingExtension
+    {
+        public static IServiceCollection ConfigureLogging(this IServiceCollection services, IConfiguration configuration, bool runLocal, string apiName)
+        {
+            var resourceAttributes = new Dictionary<string, object> {
+                { "service.name", apiName },
+                { "service.namespace", "ftw-ventures" }
+            };
+
+            var resourceBuilder = ResourceBuilder.CreateDefault().AddAttributes(resourceAttributes);
+
+            services.AddOpenTelemetryTracing(
+                t =>
+                {
+                    t.SetResourceBuilder(resourceBuilder);
+                    t.AddHttpClientInstrumentation();
+                    t.AddSqlClientInstrumentation(o => {
+                        o.SetDbStatementForText = true;
+                    });
+                    t.AddAspNetCoreInstrumentation();
+                    t.AddHotChocolateInstrumentation();
+                    if (runLocal)
+                    {
+                        t.AddConsoleExporter();
+                    }
+                    else
+                    { 
+                        t.AddAzureMonitorTraceExporter(m => m.ConnectionString = configuration["APPINSIGHTS:CONNECTIONSTRING"]);
+                    }
+                }
+            );
+
+            return services;
+        }
+    }
+}
