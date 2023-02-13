837eda3fed7a6e368b8ca763fb237b1c5092f004
Toon Vanhoutte <toon.vanhoutte@gmail.com>
2023-02-13 15:15:31 +0100

    Add random exception option

--- src/hotels-app/Ftw.Hotels.HotelPricing/Api/GraphQL/Query.cs
+++ src/hotels-app/Ftw.Hotels.HotelPricing/Api/GraphQL/Query.cs
@@ -5,8 +5,13 @@ namespace Ftw.Hotels.HotelPricing.Api.GraphQL
     public class Query
     {
         private static Random _random = new Random();
-        public RoomPriceType GetRoomPrice(Guid roomId)
+        public RoomPriceType GetRoomPrice(Guid roomId, int exceptionPercentage = 0)
         {
+            if (Random.Shared.Next(1, 100) <= exceptionPercentage)
+            {
+                throw new Exception("Failed to calculate room price, because of simulated exception.");
+            }
+
             return new RoomPriceType {
                 Price = Math.Round(_random.NextDouble() * (235 - 95) + 95, 2),
                 EarlyBirdDiscount = Math.Round(_random.NextDouble() * (0.2 - 0.01) + 0.01, 2)
